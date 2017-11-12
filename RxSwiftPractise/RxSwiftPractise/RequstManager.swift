58c02f772c0051a843a6a180f9002addc97f6d57
lieon <lieoncx@gmail.com>
2017-11-12 23:02:34 +0800

    add project rxswift practise

---
+++ RxSwiftPractise/RxSwiftPractise/RequstManager.swift
@@ -0,0 +1,21 @@
+//
+//  RequstManager.swift
+//  RxSwiftPractise
+//
+//  Created by lieon on 2017/11/12.
+//  Copyright © 2017年 Personal. All rights reserved.
+//
+
+import Foundation
+import RxSwift
+import RxCocoa
+import ObjectMapper
+import Alamofire
+
+class RequestManager {
+    static func request<T>() -> Observable<T> {
+        
+    }
+}
+
+
